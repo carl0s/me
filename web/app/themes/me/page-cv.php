c3c7027fb188cbab20032950c99296bf36f122f5
Carlo Frinolli <carlo.frinolli@nois3.it>
2015-07-03 21:33:18 +0200

    reversing the cv

--- web/app/themes/me/page-cv.php
+++ web/app/themes/me/page-cv.php
@@ -57,25 +57,26 @@ wp_reset_postdata();
     Esperienze di lavoro
   </div>
   <?php
-  if( have_rows('xp_item') ):
-    while ( have_rows('xp_item') ) : the_row();
-    ?>
+  $xp_items = get_field('xp_item');
+  $xp_reversed = array_reverse($xp_items);
+  foreach ($xp_reversed as $xp_item):
+  ?>
     <div class="job_item">
       <div class="row">
         <div class="large-3 medium-4 small-12 column years">
           <h4>
             da
-            <?php $from_yr = get_sub_field('da_anno'); $yr = explode('-', $from_yr); echo $yr[1] . '/' . $yr[0]; ?>
-            <?php if(get_sub_field('a_anno')): ?>– a <?php $to_yr = get_sub_field('a_anno'); $yr = explode('-', $to_yr); echo $yr[1] . '/' . $yr[0]; ?>
+            <?php $from_yr = $xp_item['da_anno']; $yr = explode('-', $from_yr); echo $yr[1] . '/' . $yr[0]; ?>
+            <?php if($xp_item['a_anno']): ?>– a <?php $to_yr = $xp_item['a_anno']; $yr = explode('-', $to_yr); echo $yr[1] . '/' . $yr[0]; ?>
             <?php else: ?>ad oggi<?php endif; ?>
           </h4>
         </div>
         <div class="large-9 medium-8 small-12 column job_position">
-          <h2><?php echo get_sub_field('job_title'); ?></h2>
-          <h3><?php echo get_sub_field('company'); ?></h3>
-          <div class="position_desc"><?php echo get_sub_field('job_position'); ?></div>
+          <h2><?php echo $xp_item['job_title']; ?></h2>
+          <h3><?php echo $xp_item['company']; ?></h3>
+          <div class="position_desc"><?php echo $xp_item['job_position']; ?></div>
           <div class="image">
-            <?php $image = get_sub_field('logo'); ?>
+            <?php $image = $xp_item['logo']; ?>
             <?php if($image != ''): ?><a href="<?php the_sub_field('company_link'); ?>"><img src="<?php echo $image['sizes']['medium']; ?>" alt="<?php the_sub_field('company'); ?>"></a><?php endif; ?>
           </div>
           <div class="fotorama">
@@ -84,7 +85,7 @@ wp_reset_postdata();
               while ( have_rows('esempi') ) : the_row();
               ?>
               <div class="gallery_item">
-                <?php $image = get_sub_field('immagine'); ?>
+                <?php $image = $xp_item['immagine']; ?>
                 <?php if($image != ''): ?><img src="<?php echo $image['sizes']['medium']; ?>" alt="<?php the_sub_field('company'); ?>"><?php endif; ?>
                 <figcaption>
                   <?php the_sub_field('descrizione'); ?>
@@ -99,8 +100,7 @@ wp_reset_postdata();
     </div>
   </div>
   <?php
-endwhile;
-endif;
+  endforeach;
 ?>
 </section>
 <?php
