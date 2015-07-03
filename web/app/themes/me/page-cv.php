5ecc7e843cdb699f38fd3f66189d8d3078f8bdcb
carlo frinolli <carlo.frinolli@nois3.it>
2015-07-03 17:22:43 +0200

    if

--- web/app/themes/me/page-cv.php
+++ web/app/themes/me/page-cv.php
@@ -71,7 +71,7 @@ wp_reset_postdata();
               ?>
               <div class="gallery_item">
                 <?php $image = get_sub_field('immagine'); ?>
-                <?php if($image): ?><img src="<?php echo $image['sizes']['medium']; ?>" alt="<?php the_sub_field('company'); ?>"><?php endif; ?>
+                <?php if($image != ''): ?><img src="<?php echo $image['sizes']['medium']; ?>" alt="<?php the_sub_field('company'); ?>"><?php endif; ?>
                 <figcaption>
                   <?php the_sub_field('descrizione'); ?>
                 </figcaption>
