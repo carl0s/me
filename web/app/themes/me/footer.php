689dc8e93062d0d4a0c41666e4ca580794e52394
Carlo Frinolli <carlo.frinolli@nois3.it>
2015-07-05 01:31:37 +0200

    send selection to twitter

--- web/app/themes/me/footer.php
+++ web/app/themes/me/footer.php
@@ -4,5 +4,9 @@
     </div>
   </footer>
   <?php wp_footer(); ?>
+  <script>var twitterAccount = "carl0s_";</script>
+      <div class="MBLSharetip" id="MBLSharetip">
+        <div class="tooltipContainer"><a id="sendToTwitter" href="" class="sharingLink twitter"><span></span></a></div>
+      </div>
 </body>
 </html>
--- web/app/themes/me/js/scripts.js
+++ web/app/themes/me/js/scripts.js
@@ -53,6 +53,69 @@
 
       });
 
+        $(document).ready(function() {
+          textToShare = '';
+          $(document).mousemove(function(m) {
+              generateTooltipPosition();
+          });
+      });
+      $(document).mouseup(function() {
+          $(document).mousemove(function(m) {
+              generateTooltipPosition()
+          });
+          var textToShare = getTextToShare();
+          var MBLSharetip = document.getElementById("MBLSharetip");
+          if (textToShare != '') showMeTooltip();
+      });
+      $(document).click(function() {
+          var textToShare = getTextToShare();
+          var tooltipTitle = null;
+          var newTooltipTitle = $("#MBLSharetip").attr("title");
+          if (newTooltipTitle == "") return;
+          if (newTooltipTitle !== tooltipTitle) $('#MBLSharetip').animate({
+              opacity: 0
+          }, 30);
+          if (textToShare != "") showMeTooltip();
+      });
+      $(window).resize(function() {
+          if ($('#MBLSharetip').show()) {
+              $('#MBLSharetip').animate({
+                  opacity: 0
+              }, 30);
+          }
+      });
+      function showMeTooltip() {
+          var pageURL = window.location.toString();
+          var twitterLink = "https://twitter.com/intent/tweet?text=" + getTextToShare() + "&via=" + twitterAccount + "&url=" + pageURL;
+          $('#MBLSharetip').show();
+          $('#MBLSharetip').animate({
+              opacity: 1
+          }, 30);
+          $('#sendToTwitter').attr('href', twitterLink);
+      }
+      function getTextToShare() {
+          shareTxt = '';
+          if (window.getSelection) {
+              shareTxt = window.getSelection();
+              generateTooltipPosition();
+          } else if (document.getSelection) {
+              shareTxt = document.getSelection();
+              generateTooltipPosition();
+          }
+          return shareTxt;
+      }
+      function generateTooltipPosition() {
+          var selection = window.getSelection && window.getSelection();
+          if (selection && selection.rangeCount > 0) {
+              range = selection.getRangeAt(0);
+              pos = $(window).scrollTop();
+              selection_text = selection.toString(), rect = range.getBoundingClientRect();
+              $('#MBLSharetip').css({
+                  top: (rect.top + pos - 60) - 32 + 'px',
+                  left: rect.left + (rect.width / 2) + 'px',
+              });
+          }
+      }
 
       // PRELOAD
 
