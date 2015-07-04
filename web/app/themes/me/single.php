47c7db22240137ee34b87284d07e242b863f79be
Carlo Frinolli <carlo.frinolli@nois3.it>
2015-07-04 18:25:16 +0200

    various fixes

--- web/app/themes/me/single.php
+++ web/app/themes/me/single.php
@@ -48,12 +48,12 @@ get_header();
     ?>
     <div class="title-date">
       <div class="row">
-        <div class="large-2 hide-for-small column date">
+        <div class="large-2 hide-for-medium hide-for-small column date">
           <span class="day"><?php the_time('d'); ?></span>
           <span class="month"><?php the_time('F'); ?></span>
           <span class="year"><?php the_time('Y'); ?></span>
         </div>
-        <div class="large-9 small-12 column post end">
+        <div class="large-9 medium-12 small-12 column post end">
           <h2><a href="<?php echo get_the_permalink(); ?>" title="<?php echo get_the_title(); ?>"><?php echo get_the_title(); ?></a></h2>
           <div class="icon-tag tags hide-for-small"><?php the_tags( '<span>TAGS: </span>', $sep = ', ', '' ); ?></div>
         </div>
