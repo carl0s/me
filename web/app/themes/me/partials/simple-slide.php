c2da3527c2d451d6a61534932d80c7482cead3d0
Carlo Frinolli <carlo.frinolli@nois3.it>
2016-03-13 12:44:17 +0100

    fixed date for slides

--- web/app/themes/me/partials/simple-slide.php
+++ web/app/themes/me/partials/simple-slide.php
@@ -2,22 +2,23 @@
   <figure class="large-6 medium-6 small-12 column">
     <a href="<?php the_field('link_slideshare'); ?>" title="<?php echo get_the_title(); ?>"><?php the_post_thumbnail(); ?></a>
     <div class="overlay"></div>
+    <?php
+    $date_format = "d/m/Y";
+    $date = strtotime(get_field('date'));
+    ?>
+    <div class="date">
+      <?php echo date_i18n($date_format, $date); ?>
+    </div>
   </figure>
   <div class="content large-6 medium-6 small-12 column">
     <h4><?php $terms = get_the_terms(get_the_ID(),'occasion'); echo (isset($terms) && !empty($terms))?$terms[0]->name:''; ?></h4>
     <h2><a href="<?php the_field('link_slideshare'); ?>" title="<?php echo get_the_title(); ?> su Slideshare"><?php echo get_the_title(); ?></a></h2>
     <?php the_content(); ?>
-    <?php
-    $date_format = "d/m/Y";
-    $date = strtotime(get_field('date'));
-    ?>
+
     <div class="bottom-details">
       <div class="row collapse">
         <div class="tags large-6 medium-6 small-6 column">
           <?php the_tags(); ?>
-        </div>
-        <div class="date large-6 medium-6 small-6 column">
-          <?php echo date_i18n($date_format, $date); ?>
         </div>
       </div>
     </div>
