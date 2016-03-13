67df6d1131afd7c029a5b5cb05ba61f4547257c4
Carlo Frinolli <carlo.frinolli@nois3.it>
2016-03-13 12:34:01 +0100

    updated wordpress

---
+++ web/app/themes/me/partials/simple-contact.php
@@ -0,0 +1,11 @@
+<div class="container-form-button">
+  <div class="contact-button">
+    <span class="first-text">
+      <span class="text icon-mail left">Contattami</span>
+    </span>
+    <div class="container-form">
+      <span class="title">Posso esserti utile?</span>
+    </div>
+  </div>
+</div>
+<div class="overlay-form"></div>
